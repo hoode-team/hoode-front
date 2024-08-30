7a7a03dda2da89cbe79205f8ea014da9057debe8
邱晨 <374922831@qq.com>
2024-08-30 15:51:30 +0800

    add axios

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
@@ -9,6 +11,15 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
-    }
+    },
+    plugins: [
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false // css in js
+          })
+        ]
+      })
+    ]
   })
 )
